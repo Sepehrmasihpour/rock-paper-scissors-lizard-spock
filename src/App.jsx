325c8d5fa224a2b93ffe1810e728aa44ce262b52
sepehr masihpour <sepehr.masihpour1381@gmail.com>
2023-09-08 23:23:16 +0330

    completed the options compomnent
    now whenever the player click on one of the options the state playerChoice changer to that options name in string and using the useeffect whenever the value of the playerChoice changes the state of the playerChoosing sate turns to false and the conponent that will be the result component will be rendered.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ScoreBoard from "./components/ScoreBoard.jsx";
 import Options from "./components/Options.jsx";
 import Rules from "./components/Rules.jsx";
 import "./App.scss";
 
 function App() {
-  const [score, setSCore] = useState(0);
-  const [playerChoosing, setChoosing] = useState(true);
+  const [score, setSCore] = useState(0); //the overall score of the player
+  const [playerChoosing, setChoosing] = useState(true); //for conditionall rendering if the player is choosing or has chosen
+  const [showRules, setShow] = useState(false); //if true the rules will show
+  const [choice, setchoice] = useState(""); //the option that the player has chosen
+  const [appChoice, setAppChoice] = useState(""); //the option that the app has chosen
+
+  // the options the player can choose and the options that they can beat
+  const options = {
+    scissors: ["paper", "lizard"], //example: scissors beats paper and lizard
+    paper: ["rock", "spock"],
+    rock: ["scissors", "lizard"],
+    lizard: ["spock", "paper"],
+    spock: ["scissors", "rock"],
+  };
+
+  useEffect(() => {
+    setChoosing((a) => !a);
+  }, [choice]);
+
+  const playerPick = (clicked) => {
+    setchoice(clicked);
+    console.log(clicked);
+  };
 
   const scorePlusOne = () => {
     setSCore((prevScore) => prevScore + 1);
@@ -14,10 +35,11 @@ function App() {
 
   return (
     <div className="container">
+      {showRules && <Rules />}
       {playerChoosing ? (
         <>
           <ScoreBoard score={score} />
-          <Options />
+          <Options playerPick={playerPick} />
           <Rules />
         </>
       ) : (
--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -6,24 +6,24 @@ import rock from "../assets/icon-rock.svg";
 import scissors from "../assets/icon-scissors.svg";
 import spock from "../assets/icon-spock.svg";
 
-function Options() {
+function Options(props) {
   return (
     <div className="Options">
       <div className="pentagon">
         <img src={pentagon} alt="" />
-        <div className="scissors">
+        <div onClick={() => props.playerPick("scissors")} className="scissors">
           <img src={scissors} alt="" />
         </div>
-        <div className="paper">
+        <div onClick={() => props.playerPick("paper")} className="paper">
           <img src={paper} alt="" />
         </div>
-        <div className="rock">
+        <div onClick={() => props.playerPick("rock")} className="rock">
           <img src={rock} alt="" />
         </div>
-        <div className="lizard">
+        <div onClick={() => props.playerPick("lizard")} className="lizard">
           <img src={lizard} alt="" />
         </div>
-        <div className="spock">
+        <div onClick={() => props.playerPick("spock")} className="spock">
           <img src={spock} alt="" />
         </div>
       </div>
