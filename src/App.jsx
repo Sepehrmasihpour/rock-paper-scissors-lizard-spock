eba38b1295d09351b8bd0c4dd83c5919bcad243f
sepehr masihpour <sepehr.masihpour1381@gmail.com>
2023-09-16 15:09:31 +0330

    made some adjustment and added comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
+// Importing necessary libraries and components for the app
 import { useEffect, useState } from "react";
 import Top from "./components/Top.jsx";
 import Middle from "./components/Middle.jsx";
 import Rules from "./components/Rules.jsx";
 import Bottom from "./components/Bottom.jsx";
+
+// Importing game option images
 import lizard from "./assets/icon-lizard.svg";
 import paper from "./assets/icon-paper.svg";
 import rock from "./assets/icon-rock.svg";
 import scissors from "./assets/icon-scissors.svg";
 import spock from "./assets/icon-spock.svg";
+
+// Importing styles
 import "./App.scss";
 
-// Game options defined outside of the App component
+// Array of game options, each with a name, image, and an array of options it can defeat
 const gameOptions = [
   { name: "scissors", image: scissors, canDefeat: ["paper", "lizard"] },
   { name: "paper", image: paper, canDefeat: ["rock", "spock"] },
@@ -19,37 +24,32 @@ const gameOptions = [
   { name: "spock", image: spock, canDefeat: ["scissors", "rock"] },
 ];
 
+// Function to determine the winner, returns true if player wins
 const whoWon = (playerChoice, appChoice) => {
   let playerWon = true;
-  const choice1 = playerChoice;
-  const choice2 = appChoice;
-  const choice1Name = choice1.name;
-  const choice2CanDefeat = choice2.canDefeat;
-  if (choice1 === choice2) {
+  if (playerChoice === appChoice) {
     playerWon = false;
-  } else {
-    for (const item of choice2CanDefeat) {
-      if (item === choice1Name) {
-        playerWon = false;
-      }
-    }
   }
+  playerWon = !appChoice.canDefeat.includes(playerChoice.name);
   return playerWon;
 };
 
+// Function to increase the score if player wins
 const scoreIncrease = (playerWon, setScore) => {
   if (playerWon) {
     setScore((prevScore) => prevScore + 1);
   }
 };
 
-// Function to select a random option
+// Function to select a random option from the game options
 const getRandomOption = (options) => {
   const randomIndex = Math.floor(Math.random() * options.length);
   return options[randomIndex];
 };
 
+// Main App component
 const App = () => {
+  // State variables for the game
   const [score, setScore] = useState(0);
   const [isChoosing, setIsChoosing] = useState(true);
   const [showRules, setShowRules] = useState(false);
@@ -57,14 +57,17 @@ const App = () => {
   const [appChoice, setAppChoice] = useState("");
   const [playerWon, setPlayerWon] = useState(false);
 
+  // Increase score whenever appChoice changes
   useEffect(() => {
     scoreIncrease(playerWon, setScore);
   }, [appChoice]);
 
+  // Function to toggle rules visibility
   const toggleRules = (status) => {
     setShowRules(status);
   };
 
+  // Function to handle option click and game logic
   const handleOptionClick = (clicked) => {
     const playerChoice = gameOptions.find((option) => option.name === clicked);
     const appChoice = getRandomOption(gameOptions);
@@ -74,10 +77,12 @@ const App = () => {
     setPlayerWon(whoWon(playerChoice, appChoice));
   };
 
-  const PlayAgain = () => {
+  // Function to reset the game for another round
+  const playAgain = () => {
     setIsChoosing(true);
   };
 
+  // Render the game components
   return (
     <>
       {showRules && <Rules closeRules={toggleRules} />}
@@ -90,7 +95,7 @@ const App = () => {
           appChoice={appChoice}
           options={gameOptions}
           playerWon={playerWon}
-          playAgain={PlayAgain}
+          playAgain={playAgain}
         />
         <Bottom openRules={toggleRules} />
       </div>
